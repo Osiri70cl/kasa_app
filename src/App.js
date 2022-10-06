e3b734260863d8ba75cef8c9767075051047dfa9
Remi Pince <remi.pince@outlook.com>
2022-10-06 15:53:58 +0200

    Mise en place de la page Erreur 404

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Error from "./pages/Error";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* path="*" permet de renvoyé vers la page 404 ou vers une page prédéfinit
+      si l'utilisateur se "perd" */}
+        <Route path="*" element={<Error />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/pages/About.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>A propos</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+        cumque, nisi quos optio alias architecto deserunt perspiciatis tempore
+        commodi facere, necessitatibus aspernatur tenetur laborum et!
+        Voluptatibus modi reiciendis asperiores doloribus tenetur pariatur.
+        Deleniti vel cum quo odio dolore hic dolorem enim, assumenda corporis
+        magnam, quia unde quae ex voluptas a? Totam, magnam ex ratione voluptate
+        quisquam magni quia eligendi alias harum molestiae veniam officiis
+        dolores similique maiores neque unde ab iste nisi autem temporibus
+        asperiores quas, architecto beatae excepturi. Repudiandae ad
+        reprehenderit odio, sint iure recusandae ipsum hic veritatis
+        consequuntur totam fugiat, odit distinctio voluptate earum quo, ducimus
+        nobis! Atque?
+      </p>
+    </div>
+  );
+};
+
+export default About;
